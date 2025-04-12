96165614e53c3ea2db90bcc98805ef3e7cc8482a
mohamedtba <mohamedtba50@gmail.com>
2025-04-12 11:03:12 +0100

    Add files via upload

---
+++ info.js
@@ -0,0 +1,116 @@
+
+let title=document.getElementById('title')
+let btn=document.getElementById('btn')
+let tbody=document.getElementById('tbody')
+let inpcrd=document.getElementById('credit')
+let arr=[]
+let totall=document.getElementById('total')
+
+let are=[]
+are=JSON.parse(localStorage.clients)
+let j=JSON.parse(localStorage.infoclient);
+j=+j;
+title.innerHTML=`${j+1}- ${are[j]}`
+let a=are[j];
+
+if(localStorage.getItem(a) != null){
+
+    arr=JSON.parse(localStorage.getItem(a))
+}
+
+
+btn.onclick=function(){
+if(inpcrd.value!='' && inpcrd.value >0){
+arr.push(inpcrd.value)
+localStorage.setItem(`${a}`,JSON.stringify(arr))
+inpcrd.value=''
+Show()
+totall.innerHTML=`المجموع : ${total()} دج`
+check()
+}
+
+
+}
+Show()
+totall.innerHTML=`المجموع : ${total()} دج`
+check()
+function Show() {
+    let table = "";
+    if(arr ==''){
+  table='';
+  
+  
+    }
+    else{
+    for (let i = 0; i < arr.length; i++) {
+      table += `
+  <tr>
+      <td >${arr[i]} دج</td>
+      
+      
+
+      <td><button id="${i}" class="btnd" onclick="sure(this.id)">حذف</button></td>
+   
+  </tr>
+  `;
+  }   }
+  tbody.innerHTML = table;
+      
+  
+    
+  
+  }
+  function delet(id){
+ 
+
+    arr.splice(id,1);
+    localStorage.setItem(`${are[j]}`,JSON.stringify(arr))
+    Show()
+      totall.innerHTML=`المجموع : ${total()} دج`
+    check()
+    }
+function total(){
+    let s=0;
+for(let i=0;i<arr.length;i++){
+    arr[i]=+arr[i]
+s=s+arr[i];
+
+}
+return s;
+
+}
+function check(){
+if(total()==0){
+totall.style.backgroundColor='green'
+totall.style.display='inline'
+totall.style.borderRadius='10px'
+}
+else{
+  totall.style.backgroundColor='red'
+  totall.style.display='inline'
+  totall.style.borderRadius='10px'
+}
+}
+let suree=document.getElementById('sure')
+let btnyes=document.getElementById('btnyes')
+let btnno=document.getElementById('btnno')
+function sure(id){
+  suree.style.display='block'
+  localStorage.setItem('idremove',JSON.stringify(id))
+  
+  }
+  btnyes.onclick=function(){
+  id=JSON.parse(localStorage.idremove);
+  
+  arr.splice(id,1);
+  localStorage.setItem(`${are[j]}`,JSON.stringify(arr))
+  Show()
+    totall.innerHTML=`المجموع : ${total()} دج`
+  check()
+    suree.style.display='none'
+  }
+  btnno.onclick=function(){
+  
+  suree.style.display='none'
+  
+  }
---
+++ script.js
@@ -0,0 +1,102 @@
+let inpclient = document.getElementById("client");
+let btn = document.getElementById("btn");
+let tbody = document.getElementById("tbody");
+let arr;
+let first = document.querySelector(".first");
+let sr = document.getElementById("search");
+let second = document.querySelector(".second");
+if (localStorage.clients != null) {
+  arr = JSON.parse(localStorage.clients);
+} else {
+  arr = [];
+}
+btn.onclick = function () {
+  if (inpclient.value != "") {
+    arr.push(inpclient.value);
+    localStorage.setItem("clients", JSON.stringify(arr));
+    inpclient.value = "";
+  }
+  Show();
+};
+Show();
+function Show() {
+  let table = "";
+  if (arr == "") {
+    table = "";
+  } else {
+    for (let i = 0; i < arr.length; i++) {
+      table += `
+  <tr>
+      <td >${i + 1}</td>
+      
+      <td> ${arr[i]}</td>
+
+      <td><button id="${i}" class="btn" onclick="sure(this.id)">حذف</button></td>
+      <td><button id="${i}"  class="btn" onclick="inf(this.id)">معلومات</button></td>
+  </tr>
+  `;
+    }
+  }
+  tbody.innerHTML = table;
+}
+function delet(id) {
+  localStorage.removeItem(arr[id]);
+  arr.splice(id, 1);
+  localStorage.setItem("clients", JSON.stringify(arr));
+  Show();
+}
+
+function inf(id) {
+  localStorage.setItem("infoclient", JSON.stringify(id));
+
+  location.href = "information.html";
+}
+function search() {
+  if (sr.value != "") {
+    tbody.innerHTML = "";
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].includes(sr.value)) {
+        tbody.innerHTML += `
+<tr>
+      <td >${i + 1}</td>
+      
+      <td> ${arr[i]}</td>
+
+      <td><button id="${i}" class="btn" onclick="sure(this.id)">حذف</button></td>
+      <td><button id="${i}"  class="btn" onclick="inf(this.id)">معلومات</button></td>
+  </tr>
+  
+
+
+
+`;
+      }
+    }
+  }
+  if (sr.value == "") {
+    Show();
+  }
+  
+}
+let suree=document.getElementById('sure')
+let btnyes=document.getElementById('btnyes')
+let btnno=document.getElementById('btnno')
+function sure(id){
+suree.style.display='block'
+localStorage.setItem('idremove',JSON.stringify(id))
+
+}
+btnyes.onclick=function(){
+id=JSON.parse(localStorage.idremove);
+
+  localStorage.removeItem(arr[id]);
+  arr.splice(id, 1);
+  localStorage.setItem("clients", JSON.stringify(arr));
+  Show();
+  suree.style.display='none'
+}
+btnno.onclick=function(){
+
+suree.style.display='none'
+
+}
